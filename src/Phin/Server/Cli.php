8965d342765d027a860751c5b2a7d94384d7cc30
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-05-01 22:29:12 +0200

    A better command line interface

--- src/Phin/Server/Cli.php
+++ src/Phin/Server/Cli.php
@@ -2,90 +2,148 @@
 
 namespace Phin\Server;
 
-use Phin\Server,
+use RuntimeException,
+    Phin\Server,
     Phin\Server\Handler\StaticFiles,
-    Phin\Server\Handler\Cgi;
+    Phin\Server\Handler\Cgi,
+    Symfony\Component\Console\Input,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputOption;
 
 class Cli
 {
-    protected $indexScript;
-    protected $documentRoot;
-    protected $phpCgi;
-    protected $port = 3000;
-
+    /** @var InputDefinition */
+    protected $options;
+    
     function __construct()
     {
-    }
-    
-    function setDocumentRoot($docRoot)
-    {
-        if (!is_dir($docRoot)) {
-            throw new InvalidArgumentException("Document Root is not a valid Directory");
-        }
-        $this->documentRoot = $docRoot;
-        return $this;
-    }
-    
-    function setPort($port)
-    {
-        $this->port = $port;
-        return $this;
-    }
-    
-    function setIndexScript($indexScript)
-    {
-        $this->indexScript = $indexScript;
-        return $this;
-    }
-    
-    function setPhpExecutable($php)
-    {
-        $this->phpCgi = $php;
-        return $this;
+        $this->options = new Input\InputDefinition(array(
+            new InputArgument(
+                "action", InputArgument::REQUIRED, "Possible values: start"
+            ),
+            new InputOption(
+                "document_root", 'd', 
+                InputOption::VALUE_OPTIONAL, 
+                "Directory where the server should look for files", 
+                getcwd()
+            ),
+            new InputOption(
+                "index", 'i', 
+                InputOption::VALUE_OPTIONAL,
+                "PHP File which should receive all requests for files which do not exist."
+            ),
+            new InputOption(
+                "port", 'p', 
+                InputOption::VALUE_OPTIONAL, 
+                "Port on which the server should listen", 
+                4020
+            ),
+            new InputOption(
+                "host", 'H', 
+                InputOption::VALUE_OPTIONAL, 
+                "Host on which the server should be bound", 
+                '0.0.0.0'
+            ),
+            new InputOption(
+                "php_cgi", 'P',
+                InputOption::VALUE_OPTIONAL,
+                "Override Path of PHP CGI Executable",
+                $this->findPhpCgi()
+            ),
+            new InputOption(
+                "help", 'h', 
+                InputOption::VALUE_NONE,
+                "Display this help"
+            )
+        ));
     }
     
     function run()
     {
-        if (empty($this->indexScript) and !empty($this->documentRoot)) {
-            $this->indexScript = $this->documentRoot . "/index.php";
-        
-        } else if (empty($this->documentRoot)) {
-            $this->documentRoot = dirname($this->indexScript);
+        try {
+            $input = new Input\ArgvInput(null, $this->options);
+
+            if ($action = $input->getArgument("action") !== "start") {
+                throw new RuntimeException("Action $action is not implemented");
+            }
+            if ($input->getOption("help")) {
+                throw new RuntimeException("");
+            }
+        } catch (RuntimeException $e) {
+            print $this->displayHelp();
+            return;
         }
+
+        $config = new Server\Config;
         
-        if (!is_readable($this->indexScript)) {
-            printf("FAILURE: Index script %s was not found!\r\n", $this->indexScript);
-            exit(2);
+        $documentRoot = $input->getOption("document_root");
+        if ($documentRoot != getcwd() and !$this->isAbsolute($documentRoot)) {
+            $documentRoot = realpath(getcwd() . DIRECTORY_SEPARATOR . $documentRoot);
         }
-        
-        $server = new Server(array(
-            "document_root" => $this->documentRoot,
-            "port" => $this->port
-        ));
+        $config->setDocumentRoot($documentRoot);
+
+        if (!$input->hasOption("index")) {
+            if (is_readable($documentRoot . DIRECTORY_SEPARATOR . "index.php")) {
+                $indexScript = $documentRoot . DIRECTORY_SEPARATOR . "index.php";
+            }
+        } else {
+            $indexScript = $input->getOption("index");
+            if (!$this->isAbsolute($indexScript)) {
+                $indexScript = realpath(getcwd() . DIRECTORY_SEPARATOR . $indexScript);
+            }
+        }
+
+        $config->setPort($input->getOption("port"));
+        $config->setHost($input->getOption("host"));
+
+        $server = new Server($config);
         
         $staticHandler = new StaticFiles;
+        $server->run($staticHandler);
         
-        $phpCgi = $this->findPhpCgi();
-        $cgiHandler = new Cgi($phpCgi, array(
-            "SCRIPT_FILENAME" => $this->indexScript
-        ));
+        if (!empty($indexScript)) {
+            $cgiHandler = new Cgi($input->getOption("php_cgi"), array(
+                "SCRIPT_FILENAME" => $indexScript
+            ));
+            $server->run($cgiHandler);
+        }
         
-        $server->run($staticHandler)
-               ->run($cgiHandler);
-        
-        $config = $server->getConfig();
-         
         $version = Server::VERSION;
-        $socket  = sprintf("%s:%d", $config->getHost(), $this->port);
+        $socket  = sprintf("%s:%d", $config->getHost(), $config->getPort());
         
         print <<<EOL
 >>> Welcome to Phin v$version!
->>> Using CGI Handler with $phpCgi.
+>>> Using CGI Handler with {$input->getOption('php_cgi')}.
 >>> Listening on $socket.
 >>> Terminate with [ CTRL ] + [ C ].\r\n\r\n
 EOL;
         
         $server->listen();
+    }
+
+    protected function displayHelp()
+    {   
+        $synopsis = $this->options->getSynopsis();
+        $bin = basename($_SERVER["SCRIPT_FILENAME"]);
+
+        $help = "Usage: $bin $synopsis\r\n";
+
+        $help .= "\r\nArguments:\r\n";
+        foreach ($this->options->getArguments() as $argument) {
+            $help .= sprintf("\t%s: %s\r\n", $argument->getName(), $argument->getDescription());
+        }
+        
+        $help .= "\r\nOptions:\r\n";
+        foreach ($this->options->getOptions() as $option) {
+            $help .= sprintf(
+                "\t--%s, -%s: %s\r\n", 
+                $option->getName(), 
+                $option->getShortcut(),
+                $option->getDescription()
+            );
+        }
+
+        return $help;
     }
     
     /**
@@ -95,10 +153,6 @@ EOL;
      */
     protected function findPhpCgi()
     {
-        if (is_executable($this->phpCgi)) {
-            return $this->phpCgi;
-        }
-    
         $suffix = '';
 
         if ("WIN" == strtoupper(substr(PHP_OS, 0, 3))) {
@@ -116,5 +170,15 @@ EOL;
                 return $path . "/php-cgi" . $suffix;
             }
         }
+    }
+    
+    protected function isAbsolute($path) 
+    {
+        if ("WIN" == strtoupper(substr(PHP_OS, 0, 3))) {
+            return preg_match($path, "/[a-zA-Z]\:\\\\/");
+        } else if ($path[0] == '/') {
+            return true;
+        }
+        return false;
     }
 }
